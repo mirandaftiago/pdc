8163878a1c7e95cf5e86307a3ff27d89608251b1
Tiago Miranda <miranda.f.tiago@gmail.com>
2021-07-21 07:11:50 +0100

    Jwt Authentication logic

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
 /* 
@@ -52,9 +54,21 @@ router.post('/', [
         await user.save();
 
         //Return jsonwebtoken
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
 
-
-        res.send('User registered');
+        jwt.sign(
+            payload,
+            config.get('jwtToken'),
+            { expiresIn: 3600000 },
+            (error, token) => {
+                if(error) throw error;
+                res.json({ token });
+            });
+            
     } catch (error) {
         console.log(error.message);
         res.status(500).send('Server Error');
