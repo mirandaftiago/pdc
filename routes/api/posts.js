d32b4c4460159eaae1f1669556c279eb5c16ff81
Tiago Miranda <miranda.f.tiago@gmail.com>
2021-07-22 12:51:04 +0100

    Liked post logic

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -114,6 +114,17 @@ router.delete("/:id", auth, async (req, res) => {
  */
 router.put("/like/:id", auth, async (req, res) => {
   try {
+    const post = await Post.findById(req.params.id);
+
+    //Check if the post has already been liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).json({ msg: "Post already liked" });
+    }
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+    res.json(post.likes);
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server Error");
