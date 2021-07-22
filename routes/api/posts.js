d95c1dae6dfcf112abd5f601cc79f85355c522e7
Tiago Miranda <miranda.f.tiago@gmail.com>
2021-07-22 09:25:57 +0100

    Get all posts logic

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -43,4 +43,20 @@ router.post('/', [auth, [
     }
 );
 
+/* 
+ * @route   GET api/posts
+ * @desc    Get all posts
+ * @access  Private
+ */
+router.get('/', auth, async (req, res) => {
+        try {
+            const posts = await Post.find().sort({ date: -1 });
+            res.json(posts);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
+
 module.exports = router;
